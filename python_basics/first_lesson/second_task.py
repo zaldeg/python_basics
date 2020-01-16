2f8cd70c0469e837c36c3518b3375788ea35d34c
Aleksey Zaitcev <zald@yandex.ru>
2020-01-16 22:35:30 +0300

    delete this

--- python_basics/first_lesson/second_task.py
+++
@@ -1,5 +0,0 @@
-time = int(input("Hello, pls enter time in seconds: "))
-h = time // 3600
-m = (time - h * 3600) // 60
-s = time - h * 3600 - m * 60
-print(f"Time in hh:mm:ss is {h:02}:{m:02}:{s:02}")
--- python_basics/first_lesson/third_task.py
+++
@@ -1,4 +0,0 @@
-number = input("Hi give me your secret number!!! ")
-a = number * 2
-b = number * 3
-print(f"Answer is {int(number) + int(a) + int(b)}")
