1fe2167dc1fe4e0b236b8b965016d1d77be657ad
Aleksey Zaitcev <zald@yandex.ru>
2020-01-16 16:43:35 +0300

    first home work

---
+++ python_basics/first_lesson/third_task.py
@@ -0,0 +1,4 @@
+number = input("Hi give me your secret number!!! ")
+a = number * 2
+b = number * 3
+print(f"Answer is {int(number) + int(a) + int(b)}")
---
+++ python_basics/first_program.py
@@ -0,0 +1,19 @@
+import sys
+import os
+
+import requests
+
+
+print(sys.version)
+print(sys.executable)
+x = 2
+y = 44
+print(f"{x} * {y} / 2 = {x * y /2}")
+
+
+# name = "Nikola"
+# age = 23
+# print("My name " + name + ". My age " + str(age) + "." )
+# print("My name %s. My age %d." %(name, age))
+# print("My name {0}. My age {1}" .format(name, age))
+# print(f"My name {name}. My age {age}.")
